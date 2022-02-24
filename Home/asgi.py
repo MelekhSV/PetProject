e2d7d70afb41ccb00c887eed9c897c1c3e69af6b
Siarhei Melekh <melex.1998@mail.ru>
2022-02-24 10:50:11 +0300

    changed the structure of the project

--- Home/asgi.py
+++ Home/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for PetProject project.
+ASGI config for Project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PetProject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Project.settings')
 
 application = get_asgi_application()
