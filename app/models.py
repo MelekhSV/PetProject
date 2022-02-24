e85fbf1ffed102297d4cbce460bfe1b1f3b46720
Siarhei Melekh <melex.1998@mail.ru>
2022-02-24 13:17:43 +0300

    models.py add model Product

--- app/models.py
+++ app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=30)
+    photo = models.ImageField(blank=True)
+    description = models.CharField(max_length=300)
